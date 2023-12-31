1f53c9ae32aae82cbec73435bdfeb9e2a83e9300
Iris <39096994+IrisV3rm@users.noreply.github.com>
2023-12-30 22:55:49 -0700

    Added RadiantConnect

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,11 +1,5 @@
-﻿using System.Diagnostics;
-using System.IO;
-using System.Text.Json;
-using System.Windows;
+﻿using System.Windows;
 using CyphersWatchfulEye.InternalLogic;
-using CyphersWatchfulEye.ValorantAPI.LogManager;
-using CyphersWatchfulEye.ValorantAPI.Methods;
-using CyphersWatchfulEye.ValorantAPI.Services;
 
 namespace CyphersWatchfulEye
 {
@@ -50,97 +44,6 @@ namespace CyphersWatchfulEye
             });
         }
 
-        private static async Task ValorantHandler()
-        {
-            string logPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "AppData", "Local", "Valorant", "Saved", "Logs", "ShooterGame.log");
-            do await Task.Delay(50);
-            while (Process.GetProcessesByName("VALORANT-Win64-Shipping").Length == 0);
-            do await Task.Delay(50);
-            while (!File.Exists(logPath));
-
-            ValorantClient valorantClient = new(logPath);
-            NetHandler net = new(valorantClient);
-            LogManager valorantManager = new(net);
-
-            valorantManager.Events.Queue.OnQueueChanged += (data) =>
-            {
-                Debug.WriteLine($"OnQueueChanged: {data}");
-            };
-            valorantManager.Events.Queue.OnEnteredQueue += (data) =>
-            {
-                Debug.WriteLine($"OnEnteredQueue: {data}");
-            };
-            valorantManager.Events.Queue.OnCustomGameLobbyCreated += (data) =>
-            {
-                Debug.WriteLine($"OnCustomGameLobbyCreated: {JsonSerializer.Serialize(data)}");
-            };
-            valorantManager.Events.Queue.OnLeftQueue += (data) =>
-            {
-                Debug.WriteLine($"OnLeftQueue: {data}");
-            };
-
-            valorantManager.Events.PreGame.OnAgentLockedIn += (data) =>
-            {
-                Debug.WriteLine($"OnAgentLockedIn: {data}");
-            };
-            valorantManager.Events.PreGame.OnPreGameMatchLoaded += (data) =>
-            {
-                Debug.WriteLine($"OnPreGameMatchLoaded: {data}");
-            };
-            valorantManager.Events.PreGame.OnPreGamePlayerLoaded += (data) =>
-            {
-                Debug.WriteLine($"OnPreGamePlayerLoaded: {data}");
-            };
-            valorantManager.Events.PreGame.OnAgentSelected += (data) =>
-            {
-                Debug.WriteLine($"OnAgentSelected: {data}");
-            };
-
-            valorantManager.Events.Match.OnMapLoaded += (data) =>
-            {
-                Debug.WriteLine($"OnMapLoaded: {data}");
-            };
-            valorantManager.Events.Match.OnMatchEnded += (data) =>
-            {
-                Debug.WriteLine($"OnMatchEnded: {data}");
-            };
-            valorantManager.Events.Match.OnMatchStarted += (data) =>
-            {
-                Debug.WriteLine($"OnMatchStarted: {data}");
-            };
-
-            valorantManager.Events.Round.OnRoundStarted += (data) =>
-            {
-                Debug.WriteLine($"OnRoundStarted: {data}");
-            };
-            valorantManager.Events.Round.OnRoundEnded += (data) =>
-            {
-                Debug.WriteLine($"OnMatchEnded: {data}");
-            };
-
-            valorantManager.Events.Vote.OnVoteDeclared += (data) =>
-            {
-                Debug.WriteLine($"Vote Active: {data}");
-            };
-            valorantManager.Events.Vote.OnVoteInvoked += (data) =>
-            {
-                Debug.WriteLine($"Chose Option: {data}");
-            };
-
-            valorantManager.Events.InGame.OnUtilPlaced += (data) =>
-            {
-                Debug.WriteLine($"OnUtilPlaced: {data}");
-            };
-            valorantManager.Events.InGame.OnBuyMenuClosed += (data) =>
-            {
-                Debug.WriteLine($"OnBuyMenuClosed: {data}");
-            };
-            valorantManager.Events.InGame.OnBuyMenuOpened += (data) =>
-            {
-                Debug.WriteLine($"OnBuyMenuOpened: {data}");
-            };
-        }
-
         public MainWindow()
         {
             InitializeComponent();
@@ -154,7 +57,6 @@ namespace CyphersWatchfulEye
                 SetupDictionaries();
 
                 SetPlayerControlItem(LeftPlayerElements[0], "LeftPlayer1AgentName", ContentProperty, "test");
-                await Task.Run(ValorantHandler);
             };
         }
     }
